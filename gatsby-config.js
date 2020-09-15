e5a5bf797f7d8f1e488bff4e8dc3dfeaeb8f829a
Stephan Loh <sloh4137@gmail.com>
2020-09-15 16:23:56 -0400

    Add eslint

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,12 @@
  */
 
 module.exports = {
-  /* Your site config here */
-  plugins: [
-    `gatsby-plugin-typescript`,
-    `gatsby-plugin-typegen`,
-    `gatsby-plugin-styled-components`,
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
-  ],
+    /* Your site config here */
+    plugins: [
+        `gatsby-plugin-typescript`,
+        `gatsby-plugin-typegen`,
+        `gatsby-plugin-styled-components`,
+        `gatsby-transformer-sharp`,
+        `gatsby-plugin-sharp`,
+    ],
 }
