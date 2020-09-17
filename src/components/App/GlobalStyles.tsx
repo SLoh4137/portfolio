da101e8c70b906c0b6ab8fbb4bac322a53684559
Stephan Loh <sloh4137@gmail.com>
2020-09-17 00:12:04 -0400

    Add global styles and update readme

--- src/components/App/GlobalStyles.tsx
+++ src/components/App/GlobalStyles.tsx
@@ -3,6 +3,12 @@ import { createGlobalStyle } from "styled-components"
 import { cssVar } from "utils/index"
 
 const GlobalStyles = createGlobalStyle`
+    *, *:before, *:after {
+        box-sizing: border-box;
+        font-family: Futura, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+        Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+
     body {
         background: ${cssVar("colorBackground")};
         color: ${cssVar("colorText")};
