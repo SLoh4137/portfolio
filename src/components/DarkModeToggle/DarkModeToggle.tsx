91760b86ba014b1be6f44408b19fb3e02328b168
Stephan Loh <sloh4137@gmail.com>
2020-09-17 20:15:24 -0400

    Format

--- src/components/DarkModeToggle/DarkModeToggle.tsx
+++ src/components/DarkModeToggle/DarkModeToggle.tsx
@@ -2,23 +2,23 @@ import React from "react"
 
 import { ThemeContext } from "components/App/ThemeContext"
 
-export default function Toggle() {
+export default function DarkModeToggle() {
     const { colorMode, setColorMode } = React.useContext(ThemeContext)
 
     if (!colorMode || !setColorMode) return null
 
     return (
         <>
-        <label>
-            <input
-                type="checkbox"
-                checked={colorMode === "dark"}
-                onChange={event => {
-                    setColorMode(event.target.checked ? "dark" : "light")
-                }}
-            />{" "}
-            Dark
-        </label>
+            <label>
+                <input
+                    type="checkbox"
+                    checked={colorMode === "dark"}
+                    onChange={event => {
+                        setColorMode(event.target.checked ? "dark" : "light")
+                    }}
+                />{" "}
+                Dark
+            </label>
         </>
     )
 }
