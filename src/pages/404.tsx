6b7107ee896bf511760c86ff31145b046cd99f6c
Stephan Loh <sloh4137@gmail.com>
2020-09-21 20:38:50 -0400

    Format style

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -5,6 +5,8 @@ import { Image } from "components/Image"
 import { Container } from "components/PageLayout"
 import { Heading } from "components/Typography"
 
+import { cssVar } from "utils/index"
+
 type NotFoundPageProps = PageProps & {
     data: GatsbyTypes.NotFoundPageQuery
 }
@@ -13,8 +15,16 @@ export default function NotFoundPage(props: NotFoundPageProps) {
     const { data } = props
     return (
         <>
-            <Heading size={1} textAlign="center">Oops sorry that page doesn't exist!</Heading>
-            <Heading size={2} textAlign="center">Here's a funny sign I found while biking instead</Heading>
+            <Heading size={1} textAlign="center">
+                Oops sorry that page doesn't exist!
+            </Heading>
+            <Heading
+                size={2}
+                textAlign="center"
+                color={cssVar("color-primary")}
+            >
+                Here's a funny sign I found while biking instead
+            </Heading>
             <Container maxWidth="md">
                 <Image
                     image={data.file}
